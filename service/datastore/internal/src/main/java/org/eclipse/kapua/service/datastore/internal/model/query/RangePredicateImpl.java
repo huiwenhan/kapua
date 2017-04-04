fc38043e2ec590c06f1351d457f6e56428f64373
coduz <alberto.codutti@eurotech.com>
2017-04-04 16:41:30 +0200

    Implemented new object factory for StorablePredicates
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/datastore/internal/src/main/java/org/eclipse/kapua/service/datastore/internal/model/query/RangePredicateImpl.java
+++ service/datastore/internal/src/main/java/org/eclipse/kapua/service/datastore/internal/model/query/RangePredicateImpl.java
@@ -21,22 +21,20 @@ import org.eclipse.kapua.service.datastore.model.query.StorableField;
  * @since 1.0
  *
  */
-public class RangePredicateImpl implements RangePredicate
+public class RangePredicateImpl<V extends Comparable<V>> implements RangePredicate<V>
 {
     private StorableField field;
-    private Object        minValue;
-    private Object        maxValue;
-
-    private <V extends Comparable<V>> void checkRange(Class<V> clazz) throws KapuaException
-    {
-        if (minValue == null || maxValue == null)
-            return;
-
-        V min = clazz.cast(minValue);
-        V max = clazz.cast(maxValue);
-        if (min.compareTo(max) > 0)
-            throw KapuaException.internalError("Min value must not be graeter than max value");
-    }
+    private V        minValue;
+    private V        maxValue;
+//
+//    private <V extends Comparable<V>> void checkRange() throws KapuaException
+//    {
+//        if (minValue == null || maxValue == null)
+//            return;
+//
+//        if (minValue.compareTo(maxValue) > 0)
+//            throw KapuaException.internalError("Min value must not be graeter than max value");
+//    }
 
     /**
      * Default constructor
@@ -51,7 +49,7 @@ public class RangePredicateImpl implements RangePredicate
      * @param minValue
      * @param maxValue
      */
-    public <V extends Comparable<V>> RangePredicateImpl(StorableField field, V minValue, V maxValue)
+    public  RangePredicateImpl(StorableField field, V minValue, V maxValue)
     {
         this.field = field;
         this.minValue = minValue;
@@ -64,69 +62,15 @@ public class RangePredicateImpl implements RangePredicate
         return this.field;
     }
 
-    /**
-     * Get the field
-     * 
-     * @param field
-     * @return
-     */
-    public RangePredicate setField(StorableField field)
-    {
-        this.field = field;
-        return this;
-    }
-
     @Override
-    public Object getMinValue()
+    public V getMinValue()
     {
         return minValue;
     }
 
     @Override
-    public <V extends Comparable<V>> V getMinValue(Class<V> clazz)
-    {
-        return clazz.cast(minValue);
-    }
-
-    /**
-     * Set the minimum value (typed)
-     * 
-     * @param clazz
-     * @param minValue
-     * @return
-     * @throws KapuaException
-     */
-    public <V extends Comparable<V>> RangePredicate setMinValue(Class<V> clazz, V minValue) throws KapuaException
-    {
-        this.minValue = minValue;
-        checkRange(clazz);
-        return this;
-    }
-
-    @Override
-    public Object getMaxValue()
+    public V getMaxValue()
     {
         return maxValue;
-    }
-
-    @Override
-    public <V extends Comparable<V>> V getMaxValue(Class<V> clazz)
-    {
-        return clazz.cast(maxValue);
-    }
-
-    /**
-     * Set the maximum value (typed)
-     * 
-     * @param clazz
-     * @param maxValue
-     * @return
-     * @throws KapuaException
-     */
-    public <V extends Comparable<V>> RangePredicate setMaxValue(Class<V> clazz, V maxValue) throws KapuaException
-    {
-        this.maxValue = maxValue;
-        checkRange(clazz);
-        return this;
     }
 }
